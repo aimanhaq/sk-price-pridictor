43a7e23cbae5f7f87c4e31cc917a5a906ebe7309
aimanhaq <127298561+aimanhaq@users.noreply.github.com>
2023-03-13 16:47:44 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -26,16 +26,15 @@ def predict():
     prediction=modelDTS.predict(pd.DataFrame(columns=['Brand', 'Premium' ,'Size', 'Cond'],data=np.array([Brand,Premium,Size,Cond]).reshape(1, 4)))
     dts = str(np.round(prediction[0],0)) 
    
-    # predictionPrice=modelPrice.predict(pd.DataFrame(columns=['Brand', 'Premium' ,'Size', 'Cond', 'DTS'],data=np.array([Brand,Premium,Size,Cond,10]).reshape(1, 5)))
-    # price = str(np.round(predictionPrice[0],0)) 
+    predictionPrice=modelPrice.predict(pd.DataFrame(columns=['Brand', 'Premium' ,'Size', 'Cond', 'DTS'],data=np.array([Brand,Premium,Size,Cond,10]).reshape(1, 5)))
+    price = str(np.round(predictionPrice[0],0)) 
   
-    ans = "Days To Send: "+ dts
-    #+" , "+ "Price: "+ price
+    ans = dts + " , " + price
     return ans
 
 
 if __name__=='__main__':
-    app.run(debug=False,host="0.0.0.0")
+    app.run()
 
 
 #/predict?Brand=Nike&Premium=0&Size=42&Cond=Nine
